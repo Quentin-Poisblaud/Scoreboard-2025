8e975cdbdbc9d7992d2c22a435a09c72b7c92b88
Quentin Poisblaud <quentin.poisblaud17@orange.fr>
2024-11-19 11:38:40 +0100

    fix core

--- api-rest-example/server.js
+++ api-rest-example/server.js
@@ -2,6 +2,16 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*") // Or specific origin
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    )
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    next()
+})
+
 app.use(express.json())
 
 //========== Datas ==========//
